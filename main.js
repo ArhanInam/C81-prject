0a222236b12e9cc86761e6cd3b639e6f99444d06
Arhan Inam <91776437+ArhanInam@users.noreply.github.com>
2022-05-05 08:00:41 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+canvas.getElementById("Mycanvas");
+ctx.canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokestyle= "red";
+ctx.lineWidth= 5;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokestyle= "blue";
+ctx.linewidth= 5
+ctx.arc(280, 210, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokestyle= "blue";
+ctx.linewidth= 5
+ctx.arc(310, 210, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokestyle= "blue";
+ctx.linewidth= 5
+ctx.arc(290, 240, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokestyle= "blue";
+ctx.linewidth= 5
+ctx.arc(315, 240, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokestyle= "blue";
+ctx.linewidth= 5
+ctx.arc(325, 240, 40, 0, 2*Math.PI);
+ctx.stroke();
